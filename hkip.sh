a75abb56cea4f1f78c5696785b6656c49b64e34a
Tong Dongdong <78608892@qq.com>
2024-12-02 11:08:59 +0800

    Add files via upload

---
+++ hkip.sh
@@ -0,0 +1,96 @@
+########################################################################
+                        IP质量体检报告：96.9.*.*
+                    bash <(curl -sL IP.Check.Place)
+                   https://github.com/xykt/IPQuality
+        报告时间：2024-12-02 11:00:22 CST  脚本版本：v2024-11-09
+########################################################################
+一、基础信息（Maxmind 数据库）
+自治系统号：            AS133752
+组织：                  LEASEWEB HONG KONG LIMITED
+坐标：                  114°9′57″E, 22°15′28″N
+地图：                  https://check.place/22.2578,114.1657,15,cn
+城市：                  N/A
+使用地：                [HK]香港, [AS]亚洲
+注册地：                [US]美国
+时区：                  Asia/Hong_Kong
+IP类型：                 广播IP 
+二、IP类型属性
+数据库：      IPinfo    ipregistry    ipapi     AbuseIPDB  IP2LOCATION 
+使用类型：     机房        机房        机房        机房        机房    
+公司类型：     商业        机房        机房    
+三、风险评分
+风险等级：      极低         低       中等       高         极高
+SCAMALYTICS：                         38|中风险
+ipapi：        0.26%|低风险
+AbuseIPDB：    0|低风险
+IPQS：                        75|可疑IP
+Cloudflare：   0|低风险
+DB-IP：         |低风险
+四、风险因子
+库： IP2LOCATION ipapi ipregistry IPQS SCAMALYTICS ipdata IPinfo IPWHOIS
+地区：    [HK]    [HK]    [HK]    [HK]    [HK]    [HK]    [HK]    [HK]
+代理：     否      否      否      是      否      否      否      是 
+Tor：      否      否      否      否      否      否      否      否 
+VPN：      是      是      是      是      否      无      否      否 
+服务器：   是      是      是      无      否      否      是      是 
+滥用：     否      否      否      否      无      否      无      无 
+机器人：   否      否      无      否      否      无      无      无 
+五、流媒体及AI服务解锁检测
+服务商：  TikTok   Disney+  Netflix Youtube  AmazonPV  Spotify  ChatGPT 
+状态：     失败     解锁     解锁     解锁     解锁     屏蔽     仅APP  
+地区：              [HK]     [HK]     [HK]     [HK]              [HK]   
+方式：              原生     原生     原生     原生              原生   
+六、邮局连通性及黑名单检测
+本地25端口：阻断
+IP地址黑名单数据库：  有效 439   正常 429   已标记 10   黑名单 0
+========================================================================
+今日IP检测量：282；总检测量：238615。感谢使用xy系列脚本！ 
+报告链接：https://Report.Check.Place/IP/3GNITHFAB.svg
+
+########################################################################
+                 IP质量体检报告：2001:df1:801:*:*:*:*:*
+                    bash <(curl -sL IP.Check.Place)
+                   https://github.com/xykt/IPQuality
+        报告时间：2024-12-02 11:00:22 CST  脚本版本：v2024-11-09
+########################################################################
+一、基础信息（Maxmind 数据库）
+自治系统号：            AS133752
+组织：                  LEASEWEB HONG KONG LIMITED
+坐标：                  114°10′33″E, 22°17′3″N
+地图：                  https://check.place/22.2842,114.1759,15,cn
+城市：                  N/A, 香港
+使用地：                [HK]香港, [AS]亚洲
+注册地：                [HK]香港
+时区：                  Asia/Hong_Kong
+IP类型：                 原生IP 
+二、IP类型属性
+数据库：      IPinfo    ipregistry    ipapi     AbuseIPDB  IP2LOCATION 
+使用类型：     机房        机房        机房        机房        机房    
+公司类型：     机房        机房        机房    
+三、风险评分
+风险等级：      极低         低       中等       高         极高
+SCAMALYTICS：  0|低风险
+ipapi：    0.00%|极低风险
+AbuseIPDB：    0|低风险
+IPQS：                        75|可疑IP
+Cloudflare：   0|低风险
+DB-IP：         |低风险
+四、风险因子
+库： IP2LOCATION ipapi ipregistry IPQS SCAMALYTICS ipdata IPinfo IPWHOIS
+地区：    [HK]    [HK]    [HK]    [HK]    [HK]    [HK]    [HK]    [HK]
+代理：     否      否      否      是      否      否      否      否 
+Tor：      否      否      否      否      否      否      否      否 
+VPN：      否      否      否      是      否      无      是      否 
+服务器：   是      是      是      无      否      否      是      否 
+滥用：     否      否      否      否      无      否      无      无 
+机器人：   否      否      无      否      否      无      无      无 
+五、流媒体及AI服务解锁检测
+服务商：  TikTok   Disney+  Netflix Youtube  AmazonPV  Spotify  ChatGPT 
+状态：     失败     失败    仅自制    解锁     屏蔽     屏蔽     失败   
+地区：                       [HK]     [HK]                              
+方式：                       原生     原生                              
+六、邮局连通性及黑名单检测
+本地25端口：阻断
+========================================================================
+今日IP检测量：289；总检测量：238622。感谢使用xy系列脚本！ 
+报告链接：https://Report.Check.Place/IP/11IQR8V6T.svg
